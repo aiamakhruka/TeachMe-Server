3230d80f79e42a8344c8fcc1f25e3b453f61e906
Aia_makhruka <aia.makhruka@gmail.com>
2023-07-10 16:55:36 -0400

    changing the knexfile.js for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,12 @@
-module.exports={
-    client: 'mysql2',
-    connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: '123456789',
-      database: 'teachme',
-      charset: "utf8"
-    }
-  };
+require("dotenv").config();
+
+module.exports = {
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    charset: "utf8",
+  },
+};
